2fb9920a5e0f8668eb6d1860508c55edd109b0f5
nathaneast <ahrl_ahrl@naver.com>
2020-09-23 17:41:01 +0900

    google social login working

--- client/src/redux/sagas/index.js
+++ client/src/redux/sagas/index.js
@@ -6,7 +6,7 @@ import authSaga from "./authSaga";
 import dotenv from "dotenv";
 dotenv.config();
 
-axios.default.baseURL = process.env.REACT_APP_BASIC_SERVER_URL;
+axios.defaults.baseURL = process.env.REACT_APP_BASIC_SERVER_URL;
 
 export default function* rootSaga() {
   yield all([fork(authSaga)]);
