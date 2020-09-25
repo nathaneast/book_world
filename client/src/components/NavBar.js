3675c8905066bacf14869ac3f68fd08ff07bbb1d
nathaneast <ahrl_ahrl@naver.com>
2020-09-25 15:39:30 +0900

    working navBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,21 +1,44 @@
-import React from "react";
-import { Container, Nav, NavItem, NavLink } from "reactstrap";
+import React, { useState } from "react";
+import { Container, Nav, NavItem, NavBar, NavbarToggler, Collapse, Button } from "reactstrap";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+
 import { LOGIN_REQUEST } from "../redux/types";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const dispatch = useDispatch();
   const { isAuthenticated, user } = useState((state) => state.auth);
 
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const authLink = (
+    <>
+      <NavItem>
+        {user && user.name ? (
+          <Button>{user.name}</Button>
+        ) : "no user"}
+      </NavItem>
+    </>
+  );
+
   return (
     <>
-      <div>
+      <NavBar color="dark" expand="lg" className="sticky-top">
         <Container>
-          <Nav>
-            <NavItem onClick={handleLogin}></NavItem>
-          </Nav>
+          <Link to="/" className="text-white text-decoration-none">
+            BookWorld
+          </Link>
+          <NavbarToggler onClick={handleToggle} />
+          <Collapse>
+            <Nav className="ml-auto d-flex justify-content-around" navbar>
+              {isAuthenticated ? authLink : guestLink}
+            </Nav>
+          </Collapse>
         </Container>
-      </div>
+      </NavBar>
     </>
   );
 };
