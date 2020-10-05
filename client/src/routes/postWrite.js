1eeb5a798be3f6f7749ff947abc5fb431ac7ca75
nathaneast <47707076+nathaneast@users.noreply.github.com>
2020-10-05 15:39:25 +0900

    working postWrite component

--- client/src/routes/postWrite.js
+++ client/src/routes/postWrite.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Button } from "reactstrap";
 
 const postWrite = () => {
-  return <></>;
+  const [bookTitle, setBookTitle] = useState("");
+  const [modal, setModal] = useSelector(false);
+
+  const dispatch = useDispatch();
+
+  const handleToogle = () => {};
+
+  return (
+    <div>
+      <Button onClick={handleToogle}>책 검색</Button>
+    </div>
+  );
 };
 
 export default postWrite;
